8d13ddd10cd7974046fceeaa998385045f6eb9e2
13RAMAN <84949052+13RAMAN@users.noreply.github.com>
2021-07-05 00:04:25 +0530

    Create Hash1.py

---
+++ Hash1.py
@@ -0,0 +1,9 @@
+import hashlib
+
+str_data = input("Enter string to be hashed: \n")
+
+#To generate md5 of string data
+
+result = hashlib.md5(str_data.encode()).hexdigest()
+
+print('The result generated after hash
